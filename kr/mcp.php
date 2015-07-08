c21a91e2130b6c8a12a4e62f3fb792aa0542910b
minwook shin <smw0106@naver.com>
2015-07-08 23:55:36 +0900

    translation complete

--- kr/mcp.php
+++ kr/mcp.php
@@ -9,6 +9,8 @@
 * For full copyright and license information, please see
 * the docs/CREDITS.txt file.
 * translation	2015~ minwook shin @ https://github.com/minwook-shin/phpbb-korean
+* Moderator Control Panel is not translated.
+* 모더레이터 제어판은 번역하지않습니다.
 *
 */
 
