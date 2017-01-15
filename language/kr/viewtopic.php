803d36fdc7996c3a2a451a630a6423103ed117a5
minwook <minwook0106@gmail.com>
2017-01-15 21:19:57 +0900

    viewtopic rollback

--- language/kr/viewtopic.php
+++ language/kr/viewtopic.php
@@ -10,7 +10,6 @@
 * For full copyright and license information, please see
 * the docs/CREDITS.txt file.
 *
-* korean translation	2017~ minwook shin @ https://github.com/minwook-shin/phpbb-korean
 */
 
 /**
@@ -39,91 +38,90 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-'APPROVE'=> '승인',
-'ATTACHMENT'=> '첨부 파일',
-'ATTACHMENT_FUNCTIONALITY_DISABLED'=> '첨부 파일 기능이 사용 중지되었습니다.',
+	'APPROVE'								=> 'Approve',
+	'ATTACHMENT'						=> 'Attachment',
+	'ATTACHMENT_FUNCTIONALITY_DISABLED'	=> 'The attachments feature has been disabled.',
 
-'BOOKMARK_ADDED'=> '북 마킹 된 주제입니다.',
-'BOOKMARK_ERR'=> '주제를 북마크에 저장하지 못했습니다. 다시 시도하십시오. ',
-'BOOKMARK_REMOVED'=> '북마크 된 주제를 성공적으로 제거했습니다.',
-'BOOKMARK_TOPIC'=> '북마크 주제',
-'BOOKMARK_TOPIC_REMOVE'=> '북마크에서 제거',
-'BUMPED_BY'=> '%2$s의 %1$s에 마지막으로 충돌했습니다.',
-'BUMP_TOPIC'=> '범프 주제',
+	'BOOKMARK_ADDED'		=> 'Bookmarked topic successfully.',
+	'BOOKMARK_ERR'			=> 'Bookmarking the topic failed. Please try again.',
+	'BOOKMARK_REMOVED'		=> 'Removed bookmarked topic successfully.',
+	'BOOKMARK_TOPIC'		=> 'Bookmark topic',
+	'BOOKMARK_TOPIC_REMOVE'	=> 'Remove from bookmarks',
+	'BUMPED_BY'				=> 'Last bumped by %1$s on %2$s.',
+	'BUMP_TOPIC'			=> 'Bump topic',
 
-'CODE'=> 'Code',
+	'CODE'					=> 'Code',
 
-'DELETE_TOPIC'=> '주제 삭제',
-'DELETED_INFORMATION'=> '%2$s의 %1$s에 의해 삭제되었습니다',
-'DISAPPROVE'=> '비승인',
-'DOWNLOAD_NOTICE'=> '이 게시물에 첨부 된 파일을 볼 수있는 권한이 없습니다.',
+	'DELETE_TOPIC'			=> 'Delete topic',
+	'DELETED_INFORMATION'	=> 'Deleted by %1$s on %2$s',
+	'DISAPPROVE'					=> 'Disapprove',
+	'DOWNLOAD_NOTICE'		=> 'You do not have the required permissions to view the files attached to this post.',
 
-'EDITED_TIMES_TOTAL'=> array(
-1 => '%3$s의 %2$s에 마지막으로 편집 된 %1$d 개의 시간이 수정되었습니다.',
-2 => '%3$s에 %2$s이 (가) 편집되었으며 %1$d 번 수정되었습니다.',
-),
-'EMAIL_TOPIC'=> '이메일 주제',
-'ERROR_NO_ATTACHMENT'=> '선택한 첨부 파일이 더 이상 존재하지 않습니다.',
+	'EDITED_TIMES_TOTAL'	=> array(
+		1	=> 'Last edited by %2$s on %3$s, edited %1$d time in total.',
+		2	=> 'Last edited by %2$s on %3$s, edited %1$d times in total.',
+	),
+	'EMAIL_TOPIC'			=> 'Email topic',
+	'ERROR_NO_ATTACHMENT'	=> 'The selected attachment does not exist anymore.',
 
-'FILE_NOT_FOUND_404'=> '파일 <strong> %s </ strong>이 (가) 존재하지 않습니다.',
-'FORK_TOPIC'=> '주제 복사',
-'FULL_EDITOR'=> '전체 편집자 &amp; 시사',
+	'FILE_NOT_FOUND_404'	=> 'The file <strong>%s</strong> does not exist.',
+	'FORK_TOPIC'			=> 'Copy topic',
+	'FULL_EDITOR'			=> 'Full Editor &amp; Preview',
 
-'LINKAGE_FORBIDDEN'=> '이 사이트를 보거나 다운로드하거나 링크 할 수있는 권한이 없습니다.',
-'LOGIN_NOTIFY_TOPIC'=> '이 주제에 대해 통보를 받았습니다. 로그인하여 보시기 바랍니다.',
-'LOGIN_VIEWTOPIC'=> '이 주제를 보시려면 보드를 등록하고 로그인해야합니다.',
+	'LINKAGE_FORBIDDEN'		=> 'You are not authorized to view, download or link from/to this site.',
+	'LOGIN_NOTIFY_TOPIC'	=> 'You have been notified about this topic, please login to view it.',
+	'LOGIN_VIEWTOPIC'		=> 'The board requires you to be registered and logged in to view this topic.',
 
-'MAKE_ANNOUNCE'=> '"공지"로 변경',
-'MAKE_GLOBAL'=> '"전체공개"로 변경',
-'MAKE_NORMAL'=> '"표준 주제"로 변경',
-'MAKE_STICKY'=> '"고정"으로 변경',
-'MAX_OPTIONS_SELECT'=> array(
-1 => '<strong> % d </strong> 옵션을 선택할 수 있습니다'.
-2 => '<strong> % d </strong> 옵션까지 선택할 수 있습니다',
-),
+	'MAKE_ANNOUNCE'				=> 'Change to “Announcement”',
+	'MAKE_GLOBAL'				=> 'Change to “Global”',
+	'MAKE_NORMAL'				=> 'Change to “Standard Topic”',
+	'MAKE_STICKY'				=> 'Change to “Sticky”',
+	'MAX_OPTIONS_SELECT'		=> array(
+		1	=> 'You may select <strong>%d</strong> option',
+		2	=> 'You may select up to <strong>%d</strong> options',
+	),
+	'MISSING_INLINE_ATTACHMENT'	=> 'The attachment <strong>%s</strong> is no longer available',
+	'MOVE_TOPIC'				=> 'Move topic',
 
-'MISSING_INLINE_ATTACHMENT'=> '첨부 파일 <strong> % s </strong>을 (를) 더 이상 사용할 수 없습니다',
-'MOVE_TOPIC'=> '주제 이동',
+	'NO_ATTACHMENT_SELECTED'=> 'You haven’t selected an attachment to download or view.',
+	'NO_NEWER_TOPICS'		=> 'There are no newer topics in this forum.',
+	'NO_OLDER_TOPICS'		=> 'There are no older topics in this forum.',
+	'NO_UNREAD_POSTS'		=> 'There are no new unread posts for this topic.',
+	'NO_VOTE_OPTION'		=> 'You must specify an option when voting.',
+	'NO_VOTES'				=> 'No votes',
 
-'NO_ATTACHMENT_SELECTED'=> '다운로드하거나 볼 첨부 파일을 선택하지 않았습니다.',
-'NO_NEWER_TOPICS'=> '이 포럼에는 새로운 주제가 없습니다.',
-'NO_OLDER_TOPICS'=> '이 포럼에는 이전 주제가 없습니다.',
-'NO_UNREAD_POSTS'=> '이 주제에 대한 새로운 읽지 않은 글이 없습니다.',
-'NO_VOTE_OPTION'=> '투표 할 때 옵션을 지정해야합니다.',
-'NO_VOTES'=> '투표 없음',
+	'POLL_ENDED_AT'			=> 'Poll ended at %s',
+	'POLL_RUN_TILL'			=> 'Poll runs till %s',
+	'POLL_VOTED_OPTION'		=> 'You voted for this option',
+	'POST_DELETED_RESTORE'	=> 'This post has been deleted. It can be restored.',
+	'PRINT_TOPIC'			=> 'Print view',
 
-'POLL_ENDED_AT'=> '설문 조사가 %s에 종료되었습니다',
-'POLL_RUN_TILL'=> '투표가 %s까지 실행됩니다',
-'POLL_VOTED_OPTION'=> '당신은 이 옵션에 투표했습니다',
-'POST_DELETED_RESTORE'=> '이 게시물이 삭제되었습니다. 그것은 복원 될 수 있습니다. ',
-'PRINT_TOPIC'=> '인쇄보기',
+	'QUICK_MOD'				=> 'Quick-mod tools',
+	'QUICKREPLY'			=> 'Quick Reply',
+	'QUOTE'					=> 'Quote',
 
-'QUICK_MOD'=> '빠른 수정 도구',
-'QUICKREPLY'=> '빠른 댓글',
-'QUOTE'=> '견적',
+	'REPLY_TO_TOPIC'		=> 'Reply to topic',
+	'RESTORE'				=> 'Restore',
+	'RESTORE_TOPIC'			=> 'Restore topic',
+	'RETURN_POST'			=> '%sReturn to the post%s',
 
-'REPLY_TO_TOPIC'=> '주제에 답장',
-'RESTORE'=> '복원',
-'RESTORE_TOPIC'=> '주제 복원',
-'RETURN_POST'=> '%s 포스트 %s으로 돌아 가기',
+	'SUBMIT_VOTE'			=> 'Submit vote',
 
-'SUBMIT_VOTE'=> '투표 제출',
+	'TOPIC_TOOLS'			=> 'Topic tools',
+	'TOTAL_VOTES'			=> 'Total votes',
 
-'TOPIC_TOOLS'=> '주제 도구',
-'TOTAL_VOTES'=> '총 투표 수',
+	'UNLOCK_TOPIC'			=> 'Unlock topic',
 
-'UNLOCK_TOPIC'=> '주제 잠금 해제',
-
-'VIEW_INFO'=> '세부 정보 게시',
-'VIEW_NEXT_TOPIC'=> '다음 주제',
-'VIEW_PREVIOUS_TOPIC'=> '이전 주제',
-'VIEW_RESULTS'=> '결과보기',
-'VIEW_TOPIC_POSTS'=> array(
-1 => '% d 개의 게시물',
-2 => '% d 개의 게시물',
-),
-'VIEW_UNREAD_POST'=> '읽지 않은 첫 번째 글',
-'VOTE_SUBMITTED'=> '투표가 취소되었습니다.',
-'VOTE_CONVERTED'=> '투표가 변경된 투표에 대해서는 지원되지 않습니다.',
+	'VIEW_INFO'				=> 'Post details',
+	'VIEW_NEXT_TOPIC'		=> 'Next topic',
+	'VIEW_PREVIOUS_TOPIC'	=> 'Previous topic',
+	'VIEW_RESULTS'			=> 'View results',
+	'VIEW_TOPIC_POSTS'		=> array(
+		1	=> '%d post',
+		2	=> '%d posts',
+	),
+	'VIEW_UNREAD_POST'		=> 'First unread post',
+	'VOTE_SUBMITTED'		=> 'Your vote has been cast.',
+	'VOTE_CONVERTED'		=> 'Changing votes is not supported for converted polls.',
 
 ));
